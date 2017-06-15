2d401f509d311eb5253ad07993c6e49f8fe34492
flaff <flaffy90@gmail.com>
2017-06-15 18:47:41 +0200

    server tests names fix

--- server_test.go
+++ server_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func CreateHelloResponseStringTest (t *testing.T) {
+func TestCreateHelloResponseString (t *testing.T) {
 	msg := "my random message"
 	expectedMsg := "my random message echo"
 	echoedMsg := CreateHelloResponseString(msg)
@@ -11,7 +11,7 @@ func CreateHelloResponseStringTest (t *testing.T) {
 	}
 }
 
-func GetRequestContentTest (t *testing.T) {
+func TestGetRequestContent (t *testing.T) {
 	mockRawbody := []byte("<Envelope xmlns=\"http://schemas.xmlsoap.org/soap/envelope/\"><Body xmlns=\"http://schemas.xmlsoap.org/soap/envelope/\"><Content>hello didney warld</Content></Body></Envelope>")
 	parsedRequest := GetRequestContent(mockRawbody)
 	expectedParsedRequest := "hello didney warld"
@@ -20,7 +20,7 @@ func GetRequestContentTest (t *testing.T) {
 	}
 }
 
-func GetRequestResponseTest (t *testing.T) {
+func TestGetRequestResponse (t *testing.T) {
 	parsedRequest := "hello didney warld"
 	expectedResponse := "<Envelope xmlns=\"http://schemas.xmlsoap.org/soap/envelope/\"><Body xmlns=\"http://schemas.xmlsoap.org/soap/envelope/\"><Content>hello didney warld echo</Content></Body></Envelope>"
 	response := GetRequestResponse(parsedRequest)
