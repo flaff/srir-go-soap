8555073d178d6ad17eed031761d65bc9a7e765ce
flaff <flaffy90@gmail.com>
2017-06-15 16:09:56 +0200

    client read msg from server correctly

--- client.go
+++ client.go
@@ -5,26 +5,33 @@ import (
 	"fmt"
 )
 
+// zwraca tresc zapytania
 func getMessage () string {
 	return "hello didney warld";
 }
 
+// zwraca url servera
 func getUrl () string {
 	return "http://127.0.0.1:8080";
 }
 
 func main () {
+	// ustaw informacje o web service
 	url := getUrl()
 	service := webservice.NewHello_PortType(url, false, nil)
 
+	// zrob request
 	message := getMessage()
-	response, err := service.SayHello(&message)
+	responsePtr, err := service.SayHello(&message)
 
-	fmt.Println("response");
-	fmt.Println(&response);
+	// zweryfikuj odpowiedz
+	response := *responsePtr
 
+	// obsluga bledu
 	if (err != nil) {
-		fmt.Println("err");
+		fmt.Println("error occured during request");
 		fmt.Println(err);
+	} else {
+		fmt.Println("response from server is: " + response);
 	}
 }
